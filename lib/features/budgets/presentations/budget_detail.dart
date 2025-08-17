6f5045a2f9d839f4e48063d68924f847404d7439
Rizky Aditya N <rizkyadityanugroho@gmail.com>
2025-08-17 21:43:45 +0700

    Budget Detail screen update

--- lib/features/budgets/presentations/budget_detail.dart
+++ lib/features/budgets/presentations/budget_detail.dart
@@ -1,22 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:we_budget/core/constants/constants.dart';
 import 'package:we_budget/core/routes/app_route.dart';
 import 'package:we_budget/features/budgets/controller/budget_controller.dart';
 import 'package:we_budget/features/budgets/models/budget_model.dart';
-import 'package:we_budget/features/budgets/presentations/upsert_budget_screen.dart';
 
-class BudgetDetail extends ConsumerWidget {
-  BudgetDetail({super.key, required this.budgetId});
+enum SampleItem { itemOne, itemTwo, itemThree }
+
+class BudgetDetail extends ConsumerStatefulWidget {
+  const BudgetDetail({super.key, required this.budgetId});
 
   static const String path = "/budget";
   final int budgetId;
+
+  @override
+  ConsumerState<BudgetDetail> createState() => _BudgetDetailState();
+}
+
+class _BudgetDetailState extends ConsumerState<BudgetDetail> {
   final formKey = GlobalKey<FormBuilderState>();
+  SampleItem? selectedMenu;
+
+  List<Widget> getMenuChildren(AsyncValue<Budget> budget) {
+    return [
+      MenuItemButton(onPressed: () {}, child: Text('Invite Others')),
+      MenuItemButton(
+        onPressed: () {
+          if (!budget.isLoading && budget.hasValue) {
+            updateBudget(ref, context, budget.value!);
+          }
+        },
+        child: Text('Update Budget'),
+      ),
+      MenuItemButton(
+        onPressed: () => _showDeleteDialog(context, ref),
+        child: Text("Delete Budget", style: TextStyle(color: Colors.red)),
+      ),
+    ];
+  }
 
   void updateBudget(WidgetRef ref, BuildContext context, Budget oldBudget) {
-    context.goNamed(
+    context.pushNamed(
       AppRoute.createBudget.name,
       extra: <String, dynamic>{
         'formKey': formKey,
@@ -27,6 +54,7 @@ class BudgetDetail extends ConsumerWidget {
               'name': name,
               'total_amount': totalAmount,
               'reset_days': resetDays,
+              'is_daily_spend': isDailySpend,
             } = formKey.currentState!.value;
             final newBudget = oldBudget.copyWith(
               startDate: startDate as DateTime,
@@ -34,6 +62,7 @@ class BudgetDetail extends ConsumerWidget {
               totalAmount: int.parse(totalAmount as String),
               modifiedAt: DateTime.now(),
               resetDay: resetDays as int?,
+              isDailySpend: isDailySpend as bool,
             );
             ref.read(budgetControllerProvider.notifier).updateBudget(newBudget);
             context.pop();
@@ -74,39 +103,101 @@ class BudgetDetail extends ConsumerWidget {
     );
 
     if (isDelete) {
-      ref.read(budgetControllerProvider.notifier).deleteBudget(budgetId);
-      context.pop();
+      ref.read(budgetControllerProvider.notifier).deleteBudget(widget.budgetId);
+      if (context.mounted) {
+        context.pop();
+      }
     }
   }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final budget = ref.watch(budgetByIdProvider(budgetId));
+  Widget build(BuildContext context) {
+    final budget = ref.watch(budgetByIdProvider(widget.budgetId));
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: Text("Budget"),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Text("Budget Detail"),
+        centerTitle: true,
         actions: [
-          IconButton(
-            onPressed: () => _showDeleteDialog(context, ref),
-            icon: Icon(Icons.delete),
+          MenuAnchor(
+            builder: (context, controller, child) => IconButton(
+              onPressed: () {
+                if (controller.isOpen) {
+                  controller.close();
+                } else {
+                  controller.open();
+                }
+              },
+              icon: Icon(Icons.more_horiz),
+            ),
+            menuChildren: getMenuChildren(budget),
           ),
         ],
       ),
       body: budget.whenOrNull(
-        data: (data) => Column(
-          children: [
-            Text("This is your ${data.name} budget"),
-            Text("Your total budget is Rp. ${data.totalAmount}"),
-            Text("Your remaining budget is Rp. xxxx"),
-            Text("${data.daysBeforeReset} days left before budget resets"),
-            Text(
-              "Your budget will resetted in ${Constants.dateFormat.format(data.resetDate)}",
-            ),
-            FilledButton(
-              onPressed: () => updateBudget(ref, context, data),
-              child: Text("Update"),
-            ),
-          ],
+        data: (data) => SafeArea(
+          top: false,
+          bottom: true,
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              final topContainerHeight = constraints.maxHeight * 0.4;
+              return Stack(
+                children: [
+                  Container(
+                    height: topContainerHeight,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: (constraints.maxWidth * 0.05) / 2,
+                    ),
+                    child: Column(
+                      children: [
+                        Center(
+                          child: SizedBox(
+                            height: topContainerHeight,
+                            child: Column(
+                              children: [
+                                Gap(constraints.maxHeight * 0.11),
+                                CircleAvatar(radius: 50),
+                                Text(data.name, style: TextStyle(fontSize: 24)),
+                                if (data.description?.isNotEmpty ?? false)
+                                  Text(
+                                    data.description!,
+                                    style: TextStyle(fontSize: 14),
+                                  ),
+                                FittedBox(
+                                  child: Text(
+                                    "Rp. 50.000 / Rp. ${Constants.formatThousandFromInt(data.totalAmount)}",
+                                    style: TextStyle(fontSize: 24),
+                                  ),
+                                ),
+                                Gap(15),
+                                Text(
+                                  "${data.daysBeforeReset} days left before resets",
+                                ),
+                                Text(
+                                  Constants.dateFormat.format(data.resetDate),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
         ),
         loading: () => Center(child: CircularProgressIndicator()),
         error: (err, st) => Center(child: Text(err.toString())),
